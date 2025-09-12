7902e5f9316971f18db01cc091c8eb45a7260c01
Amit Kumar <amitkuamr981>
2025-09-13 03:28:47 +0530

    add code for exceptional handling

---
+++ exception/custom_exception.py
@@ -0,0 +1,28 @@
+import sys
+import traceback
+from logger.custom_logger import CustomLogger
+ 
+ #deifine the logger
+logger=CustomLogger().get_logger(__file__)
+class DocumentPortalException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        _,_,exc_tb=error_detail.exc_info()
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        self.lineno=exc_tb.tb_lineno
+        self.error_message=str(error_message)
+        self.traceback_str=''.join(traceback.format_exception(*error_detail.exc_info()))
+    def __str__(self):
+        return f"""
+    error in [{self.file_name}] at line [{self.lineno}]
+    Message:str(error_message)
+    Tracceback:
+    {self.traceback_str}
+    """
+#if __name__=="__main__":
+  #  try:
+   #     a=1/0
+   #     print(a)  
+   # except Exception as e:
+   #     app_exc=DocumentPortalException(e,sys)
+    #    logger.error(app_exc)
+    #    raise app_exc
